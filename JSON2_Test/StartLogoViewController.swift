a89006d5eca6848ab60a24ecb29c11d88a25995b
mrsirapro@gmail.com <mrsirapro@gmail.com>
2018-01-18 13:24:56 +0200

    remake goBtn

--- JSON2_Test/StartLogoViewController.swift
+++ JSON2_Test/StartLogoViewController.swift
@@ -8,13 +8,22 @@
 
 import UIKit
 
-class StartLogoViewController: UIViewController {
+class StartLogoViewController: UIViewController, UIViewControllerTransitioningDelegate {
     @IBOutlet weak var goBtn: UIButton!
     
     @IBOutlet weak var LogoView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        
+        goBtn.layer.cornerRadius = goBtn.frame.size.height / 2
+        
+        goBtn.layer.shadowOffset = CGSize (width: 9, height: 9)  // 1
+        goBtn.layer.shadowOpacity = 0.7 // 2
+        goBtn.layer.shadowRadius = 5 // 3
+        goBtn.layer.shadowColor = UIColor(red: 44.0/255.0, green: 62.0/255.0, blue: 80.0/255.0, alpha: 1.0).cgColor // 4
+        
 
         // Do any additional setup after loading the view.
     }
